812fa4e445cef4b9724631860649d8362123bf67
nithisha <nithisha816@gmail.com>
2026-03-12 15:14:04 +0530

    Resolved merge conflict

--- app.py
+++ app.py
@@ -3,7 +3,5 @@ print("Hello from main branch")
 =======
 print("Hello from feature branch")
 >>>>>>> feature1
-
-Resolve and save:
 print("Hello from main branch")
 print("Hello from feature branch")
